6a1c41ef44c9264d92b987c4a6e2da06a0612573
Nandini Maity <akashnandini@gmail.com>
2018-11-27 23:48:48 -0500

    unit-4-game v1.5

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -32,24 +32,22 @@ $(document).ready(function() {
     $("#tot_score").text(tot_score);
 
     function reset(){
-        var random_number = Math.floor((Math.random() * 120) + 19);    
+        random_number = Math.floor((Math.random() * 120) + 19);    
         console.log(random_number);
         $("#random").text(random_number);
 
         //Each crystal should have a random hidden value between 1 - 12.    
 
-        var ruby = Math.floor((Math.random() * 12) + 1);
-        var sapphire = Math.floor((Math.random() * 12) + 1);
-        var purple = Math.floor((Math.random() * 12) + 1);
-        var emerald = Math.floor((Math.random() * 12) + 1);
+        ruby = Math.floor((Math.random() * 12) + 1);
+        sapphire = Math.floor((Math.random() * 12) + 1);
+        purple = Math.floor((Math.random() * 12) + 1);
+        emerald = Math.floor((Math.random() * 12) + 1);
 
-        /*console.log("ruby== "+ruby);
+        console.log("ruby== "+ruby);
         console.log("sapphire== "+sapphire);
         console.log("purple== "+purple);
-        console.log("emerald== "+emerald);*/
+        console.log("emerald== "+emerald);
         tot_score = 0;
-        win = 0;
-        loss = 0;
         $("#tot_score").text(tot_score);
         $("#wins").text(win);
         $("#losses").text(loss);   }
@@ -61,7 +59,7 @@ $(document).ready(function() {
         $("#tot_score").text(tot_score);
         console.log("tot_score== "+tot_score);
        
-        if(tot_score===random_number){
+        if(tot_score==random_number){
             win++;
             console.log("wins== "+win);
             $("#wins").text(win);
@@ -75,8 +73,6 @@ $(document).ready(function() {
             $("#msg").text("You loss!");
             reset();
         }
-        
-
     });
 
     //Sapphire crystal
@@ -85,7 +81,7 @@ $(document).ready(function() {
         tot_score = tot_score+sapphire;
         $("#tot_score").text(tot_score);
         console.log("tot_score== "+tot_score);
-        if(tot_score===random_number){
+        if(tot_score==random_number){
             win++;
             console.log("wins== "+win);
             $("#wins").text(win);
@@ -99,7 +95,6 @@ $(document).ready(function() {
             $("#msg").text("You loss!");
             reset();
         }        
-
     });
 
     //Purple crystal
@@ -108,7 +103,7 @@ $(document).ready(function() {
         tot_score = tot_score+purple;
         $("#tot_score").text(tot_score);
         console.log("tot_score== "+tot_score);
-        if(tot_score===random_number){
+        if(tot_score==random_number){
             win++;
             console.log("wins== "+win);
             $("#wins").text(win);
@@ -122,7 +117,6 @@ $(document).ready(function() {
             $("#msg").text("You loss!");
             reset();
         }        
-
     });
 
     //Emerald crystal
@@ -131,7 +125,7 @@ $(document).ready(function() {
         tot_score = tot_score+emerald;
         $("#tot_score").text(tot_score);
         console.log("tot_score== "+tot_score);
-        if(tot_score===random_number){
+        if(tot_score==random_number){
             win++;
             console.log("wins== "+win);
             $("#wins").text(win);
@@ -145,9 +139,5 @@ $(document).ready(function() {
             $("#msg").text("You loss!");
             reset();
         }       
-
     }); 
-
-    
-
 });
